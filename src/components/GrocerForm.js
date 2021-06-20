d332eb9960ca014b84853f58ba483c5315c38b6d
Pranav <pranavmendi@gmail.com>
2021-06-20 15:12:51 +0530

    Clean up the project files and add GrocerForm component

---
+++ src/components/GrocerForm.js
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from 'react';
+
+const GrocerForm = (props) => {
+	const [input, setInput] = useState('');
+	// const inputRef = useRef('');
+
+	const handleChange = (e) => {
+		setInput(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target.value);
+		// console.log(inputRef.current.value);
+		// props.onSubmit({
+		// 	id: Math.floor(Math.random() * 10000),
+		// 	item: e.target.value,
+		// });
+		setInput('');
+	};
+
+	return (
+		<form className='grocer-form' onSubmit={handleSubmit}>
+			<input
+				type='text'
+				placeholder='Add an item'
+				className='grocer-input'
+				value={input}
+				// ref={inputRef}
+				onChange={handleChange}
+			/>
+			<button className='grocer-button'>Add an item</button>
+		</form>
+	);
+};
+
+export default GrocerForm;
